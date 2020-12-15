473cc762d5d91b5277b169c03ec27e2b96ef0992
chenlipeng <chenlipeng@oneniceapp.com>
2020-12-15 23:33:53 +0800

    sort

--- usort/MergeSort.go
+++ usort/MergeSort.go
@@ -1,7 +1,49 @@
 package usort
 
 /*
+ * 归并排序（Merge Sort）
+ * 		归并排序是建立在归并操作上的一种有效的排序算法。该算法是采用分治法（Divide and Conquer）的一个非常典型的应用。将已有序的子序列合并，得到完全有序的序列；即先使每个子序列有序，再使子序列段间有序。若将两个有序表合并成一个有序表，称为2-路归并。
+ *
+ * 算法描述
+ * 		把长度为n的输入序列分成两个长度为n/2的子序列；
+ * 		对这两个子序列分别采用归并排序；
+ * 		将两个排序好的子序列合并成一个最终的排序序列。
+ */
+
+/*
  * @desc 归并排序
  */
 func MergeSort(arr []int) {
+	l := len(arr)
+	if l <= 1 {
+		return
+	}
+
+	mid := int(l / 2)
+	MergeSort(arr[0:mid])
+	MergeSort(arr[mid:])
+
+	temp := []int{}
+	i, j := 0, mid
+	for i < mid && j < l {
+		if arr[i] < arr[j] {
+			temp = append(temp, arr[i])
+			i++
+		} else {
+			temp = append(temp, arr[j])
+			j++
+		}
+	}
+
+	for ; i < mid; i++ {
+		temp = append(temp, arr[i])
+	}
+
+	for ; j < l; j++ {
+		temp = append(temp, arr[j])
+	}
+
+	for i := 0; i < l; i++ {
+		arr[i] = temp[i]
+	}
 }
--- usort/ShellSort.go
+++ usort/ShellSort.go
@@ -1,7 +1,45 @@
 package usort
 
 /*
+ * 希尔排序（Shell Sort）
+ * 		1959年Shell发明，第一个突破O(n2)的排序算法，是简单插入排序的改进版。它与插入排序的不同之处在于，它会优先比较距离较远的元素。希尔排序又叫缩小增量排序。
+ *
+ * 算法描述
+ * 		先将整个待排序的记录序列分割成为若干子序列分别进行直接插入排序，具体算法描述：
+ *
+ * 		选择一个增量序列t1，t2，…，tk，其中ti>tj，tk=1；
+ * 		按增量序列个数k，对序列进行k 趟排序；
+ * 		每趟排序，根据对应的增量ti，将待排序列分割成若干长度为m 的子序列，分别对各子表进行直接插入排序。仅增量因子为1 时，整个序列作为一个表来处理，表长度即为整个序列的长度。
+ *
+ * 算法分析
+ * 		希尔排序的核心在于间隔序列的设定。既可以提前设定好间隔序列，也可以动态的定义间隔序列。动态定义间隔序列的算法是《算法（第4版）》的合著者Robert Sedgewick提出的。
+ */
+
+/*
  * @desc Shell希尔排序
  */
 func ShellSort(arr []int) {
+	l := len(arr)
+	for step := int(l / 2); step >= 1; step = int(step / 2) {
+		insertionSort(arr, step)
+	}
+}
+
+/*
+ * @desc 可调节步长的插入排序
+ */
+func insertionSort(arr []int, step int) {
+	if len(arr) < 2 {
+		return
+	}
+
+	l := len(arr)
+	for i := step; i < l; i++ { //Tips. 这里要i++
+		pos := i - step
+		currentValue := arr[i]
+		for ; pos >= 0 && arr[pos] > currentValue; pos -= step {
+			arr[pos+step] = arr[pos]
+		}
+		arr[pos+step] = currentValue
+	}
 }
